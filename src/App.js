1aa8b2b0510eb1a5b2e7caa6f6642ad0dc65596c
Inigo Jimenez <inigo.jimenez@gmail.com>
2023-03-31 12:09:11 +0200

    app en React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import RetrasoMeter from './components/RetrasoMeter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>RetrasoMeter</h1>
+      <RetrasoMeter />
     </div>
   );
 }
