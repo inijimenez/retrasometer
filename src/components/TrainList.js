ec37307116795f49c0e252979b3cef937780fe79
Inigo Jimenez <inigo.jimenez@gmail.com>
2023-04-04 13:20:25 +0200

    seleccion fila

--- src/components/TrainList.js
+++ src/components/TrainList.js
@@ -1,9 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import {
+  Box,
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from '@mui/material';
 import { getTrains } from '../api/renfeApi';
 
 const TrainList = ({ origin, destination }) => {
   const [trains, setTrains] = useState([]);
+  const [selectedTrain, setSelectedTrain] = useState(null);
 
   useEffect(() => {
     const fetchTrains = async () => {
@@ -12,6 +23,18 @@ const TrainList = ({ origin, destination }) => {
     };
     fetchTrains();
   }, [origin, destination]);
+
+  const handleRowClick = (train) => {
+    setSelectedTrain(train);
+  };
+
+  const handleUpdateSalida = () => {
+    // Actualizar HoraSalida (REAL) en función de la hora actual
+  };
+
+  const handleUpdateLlegada = () => {
+    // Actualizar HoraLlegada (REAL) en función de la hora actual
+  };
 
   return (
     <Box>
@@ -22,26 +45,43 @@ const TrainList = ({ origin, destination }) => {
               <TableCell>Línea</TableCell>
               <TableCell>Tren</TableCell>
               <TableCell>Hora salida (EST)</TableCell>
-              <TableCell>Hora salida (REAL)</TableCell>
-              <TableCell>Hora llegada (EST)</TableCell>
-              <TableCell>Hora llegada (REAL)</TableCell>
-              <TableCell>Duración (EST)</TableCell>
-              <TableCell>Duración (REAL)</TableCell>
+              {selectedTrain && (
+                <>
+                  <TableCell>Hora salida (REAL)</TableCell>
+                  <TableCell>Hora llegada (EST)</TableCell>
+                  <TableCell>Hora llegada (REAL)</TableCell>
+                  <TableCell>Duración (EST)</TableCell>
+                  <TableCell>Duración (REAL)</TableCell>
+                </>
+              )}
             </TableRow>
           </TableHead>
           <TableBody>
-            {trains.map((train) => (
-              <TableRow key={train.cdgoTren}>
-                <TableCell>{train.linea}</TableCell>
-                <TableCell>{train.cdgoTren}</TableCell>
-                <TableCell>{train.horaSalida}</TableCell>
-                <TableCell>-</TableCell>
-                <TableCell>{train.horaLlegada}</TableCell>
-                <TableCell>-</TableCell>
-                <TableCell>{train.duracion}</TableCell>
-                <TableCell>-</TableCell>
-              </TableRow>
-            ))}
+            {trains.map((train) => {
+              if (!selectedTrain || selectedTrain.cdgoTren === train.cdgoTren) {
+                return (
+                  <TableRow key={train.cdgoTren} onClick={() => handleRowClick(train)}>
+                    <TableCell>{train.linea}</TableCell>
+                    <TableCell>{train.cdgoTren}</TableCell>
+                    <TableCell>{train.horaSalida}</TableCell>
+                    {selectedTrain && selectedTrain.cdgoTren === train.cdgoTren && (
+                      <>
+                        <TableCell>
+                          <Button onClick={handleUpdateSalida}>Actualizar Hora</Button>
+                        </TableCell>
+                        <TableCell>{train.horaLlegada}</TableCell>
+                        <TableCell>
+                          <Button onClick={handleUpdateLlegada}>Actualizar Hora</Button>
+                        </TableCell>
+                        <TableCell>{train.duracion}</TableCell>
+                        <TableCell>-</TableCell>
+                      </>
+                    )}
+                  </TableRow>
+                );
+              }
+              return null;
+            })}
           </TableBody>
         </Table>
       </TableContainer>
