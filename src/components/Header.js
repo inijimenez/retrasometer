e92ef0c8a11cdb83b81bc757e6b3ee2701131949
inigo-jimenez <inigo-jimenez@users.noreply.github-com>
2023-06-02 10:24:16 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Typography, Grid, Box } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 
 function Header() {
   return (
