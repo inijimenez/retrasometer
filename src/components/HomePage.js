cc82c0cbb8e3c8eacebe0649ba1c7cde9029abf1
inigo-jimenez <inigo-jimenez@users.noreply.github-com>
2023-06-06 16:55:24 +0200

    Update HomePage.js

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -18,75 +18,70 @@ const generateUniqueIdentifier = () => {
     const timestamp = new Date().getTime();
     const randomValue = Math.floor(Math.random() * 10000);
     return `${timestamp}-${randomValue}`;
-  };
+};
 
 
 const HomePage = () => {
-  const [origin, setOrigin] = useState(null);
-  const [destination, setDestination] = useState(null);
-  const [setUniqueIdentifier] = useState('');
+    const [origin, setOrigin] = useState(null);
+    const [destination, setDestination] = useState(null);
 
-  const handleOriginChange = (station) => {
-    setOrigin(station);
-  };
+    const handleOriginChange = (station) => {
+        setOrigin(station);
+    };
 
-  const handleDestinationChange = (station) => {
-    setDestination(station);
-  };
+    const handleDestinationChange = (station) => {
+        setDestination(station);
+    };
 
-  useEffect(() => {
-    // Comprobar si ya existe un identificador único en el almacenamiento local
-    const storedIdentifier = localStorage.getItem('uniqueIdentifier');
-    if (storedIdentifier) {
-      setUniqueIdentifier(storedIdentifier);
-    } else {
-      // Generar un nuevo identificador único
-      const newIdentifier = generateUniqueIdentifier();
-      setUniqueIdentifier(newIdentifier);
-      // Almacenar el identificador único en el almacenamiento local
-      localStorage.setItem('uniqueIdentifier', newIdentifier);
-    }
+    useEffect(() => {
+        // Comprobar si ya existe un identificador único en el almacenamiento local
+        const storedIdentifier = localStorage.getItem('uniqueIdentifier');
+        if (!storedIdentifier) {
+            // Generar un nuevo identificador único
+            const newIdentifier = generateUniqueIdentifier();
+            localStorage.setItem('uniqueIdentifier', newIdentifier);
+        }
 
-    if (process.env.NODE_ENV === 'development') {      
-      console.log("Inicializo datos DEBUG")
-      const miLocalStation = {localStations};
-      const miLocalTrain = {localTrains};
-      localStorage.setItem("stations", JSON.stringify(miLocalStation.localStations));
-      localStorage.setItem("trains", JSON.stringify(miLocalTrain.localTrains));
-    }
-  }, [setUniqueIdentifier]);
-  return (
-    <ToastProvider>
-    <Container>
-      <Grid container spacing={2} justifyContent="center">
-        <Grid item xs={12} sm={6}>
-          <StationSelect
-            label="Estación de origen"
-            value={origin}
-            onChange={handleOriginChange}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <StationSelect
-            label="Estación de destino"
-            value={destination}
-            onChange={handleDestinationChange}
-          />
-        </Grid>
-      </Grid>
-      <Box mt={4}>
-      {origin && destination && (
-        <TrainList origin={origin} destination={destination} />
-      )}
-      </Box>
-      <Link to="/stats">
-  <button>
-    <FaChartBar />
-  </button>
-</Link>
-    </Container>
-    </ToastProvider>
-  );
+        if (process.env.NODE_ENV === 'development') {
+            console.log("Inicializo datos DEBUG")
+            const miLocalStation = { localStations };
+            const miLocalTrain = { localTrains };
+            localStorage.setItem("stations", JSON.stringify(miLocalStation.localStations));
+            localStorage.setItem("trains", JSON.stringify(miLocalTrain.localTrains));
+        }
+    }, []);
+    return (
+        <ToastProvider>
+            <Container>
+                <Grid container spacing={2} justifyContent="center">
+                    <Grid item xs={12} sm={6}>
+                        <StationSelect
+                            label="Estación de origen"
+                            value={origin}
+                            onChange={handleOriginChange}
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <StationSelect
+                            label="Estación de destino"
+                            value={destination}
+                            onChange={handleDestinationChange}
+                        />
+                    </Grid>
+                </Grid>
+                <Box mt={4}>
+                    {origin && destination && (
+                        <TrainList origin={origin} destination={destination} />
+                    )}
+                </Box>
+                <Link to="/stats">
+                    <button>
+                        <FaChartBar />
+                    </button>
+                </Link>
+            </Container>
+        </ToastProvider>
+    );
 };
 
 export default HomePage;
